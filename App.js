0b71415f655d7b45cd81aab80c87e3eaea753bef
Sandeep Singh <sandeepsinghgour0@gmailo.com>
2020-08-11 20:54:42 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,104 @@
+//   Curennt location
+window.addEventListener('load', () => {
+    
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(position => {
+             console.log(position)
+            const long = position.coords.longitude;
+           const lat = position.coords.latitude;
+           
+
+
+            const api1 = ` https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=f07a8c27e854fb18f6d03ab0f4d26add`
+
+            fetch(api1).then(weather1 => {
+                return weather1.json();
+            }).then(result);
+
+            function result(weather1) {
+                console.log(weather1);
+                var temp0 = weather1.main.temp - 273;
+                var temp1 = weather1.main.temp_max - 273;
+                var temp2 = weather1.main.temp_min - 273;
+
+
+                let city = document.querySelector('.location .city');
+                city.innerText = `${weather1.name}, ${weather1.sys.country}`;
+
+                let now = new Date();
+                let date = document.querySelector('.location .date');
+                date.innerText = dateBuilder(now);
+
+                let temp = document.querySelector('.current .temp');
+                temp.textContent = `${temp0.toFixed(1)}°c`;
+
+                let weather_el = document.querySelector('.current .weather');
+                weather_el.innerText = weather1.weather[0].description;
+
+                let hilow = document.querySelector('.hi-low');
+                hilow.textContent = `${temp1.toFixed(1)} °c / ${temp2.toFixed(1)} °c`;
+            }
+        })
+
+
+    }
+});
+
+// Search by City
+
+const api = {
+    key: "f07a8c27e854fb18f6d03ab0f4d26add",
+    base: "https://api.openweathermap.org/data/2.5/"
+}
+
+const searchbox = document.querySelector('.search-box');
+searchbox.addEventListener('keypress', setQuery);
+
+function setQuery(e) {
+    if (e.keyCode == 13) {
+        getResults(searchbox.value);
+        // console.log(searchbox.value);
+    }
+}
+
+function getResults(query) {
+    fetch(`${api.base}weather?q=${query}&appid=${api.key}`)
+        .then(weather => {
+            return weather.json();
+        }).then(displayResults);
+}
+
+function displayResults(weather) {
+    var temp2 = weather.main.temp - 273;
+    var temp21 = weather.main.temp_max - 273;
+    var temp22 = weather.main.temp_min - 273;
+
+
+    let city = document.querySelector('.location .city');
+    city.innerText = `${weather.name}, ${weather.sys.country}`;
+
+    let now = new Date();
+    let date = document.querySelector('.location .date');
+    date.innerText = dateBuilder(now);
+
+    let temp = document.querySelector('.current .temp');
+    temp.textContent = `${temp2.toFixed(1)}°c`;
+
+    let weather_el = document.querySelector('.current .weather');
+    weather_el.innerText = weather.weather[0].main;
+
+    let hilow = document.querySelector('.hi-low');
+    hilow.textContent = `${temp21.toFixed(1)} °c / ${temp22.toFixed(1)} °c`;
+}
+
+function dateBuilder(d) {
+    let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+    let day = days[d.getDay()];
+    let date = d.getDate();
+    let month = months[d.getMonth()];
+    let year = d.getFullYear();
+
+    return `${day} ${date} ${month} ${year}`;
+}
